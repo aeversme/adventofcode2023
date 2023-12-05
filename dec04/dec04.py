0d8dbc64ad8003bc3c4046f22d73bcc0003bdc64
Alex Eversmeyer <aeversme@gmail.com>
2023-12-04 21:07:15 -0700

    add dec04 part 2 solution

--- dec04/dec04.py
+++ dec04/dec04.py
@@ -22,13 +22,42 @@ def score_winning_numbers(data):
 	return total_sum
 
 
+def count_winning_cards(data):
+	card_dict = {}
+	for c in range(len(data)):
+		card_dict[c + 1] = 1
+
+	for card in data:
+		print(card)
+		card_number = card[0]
+
+		winning_nums = 0
+		for wn in card[1][0]:
+			if wn in card[1][1]:
+				winning_nums += 1
+		print(f'   {winning_nums} winning numbers for card {card_number}...')
+
+		for i in range(card_number + 1, card_number + winning_nums + 1):
+			card_dict[i] += card_dict[card_number]
+
+		print(card_dict)
+
+	return card_dict
+
+
 def main():
 	with open('input.txt') as i:
 		i_raw = i.readlines()
 
 	data = convert_input(i_raw)
 
-	total_sum = score_winning_numbers(data)
+	# total_sum = score_winning_numbers(data)
+
+	total_sum = 0
+
+	card_dict = count_winning_cards(data)
+	for k, v in card_dict.items():
+		total_sum += v
 
 	return total_sum
 
