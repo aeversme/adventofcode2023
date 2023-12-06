b8801565c1357c581baa4382cc49931737e4c2e5
Alex Eversmeyer <aeversme@gmail.com>
2023-12-05 21:17:44 -0700

    add dec06 solutions

--- dec05/dec05.py
+++ dec05/dec05.py
@@ -1,19 +1,99 @@
 from input_handler import convert_input
 
 
+def seeds_to_ranges(seeds):
+	seed_ranges = []
+	for i in range(len(seeds)):
+		if i % 2 == 0:
+			start = seeds[i]
+			end = start + (seeds[i + 1] - 1)
+			seed_ranges.append([start, end])
+
+	return seed_ranges
+
+
+def convert_number(almanac, process, number):
+	new_number = 0
+	for k, v in almanac[process].items():
+		if v['source_start'] <= number <= v['source_end']:
+			return number + v['modifier']
+	if new_number == 0:
+		return number
+
+
+def reverse_convert(almanac, process, number):
+	new_number = 0
+	for k, v in almanac[process].items():
+		if v['destination_start'] <= number <= v['destination_end']:
+			return number - v['modifier']
+	if new_number == 0:
+		return number
+	return
+
+
 def main():
 	with open('input.txt') as i:
 		i_raw = i.readlines()
 
-	data = convert_input(i_raw)
+	seeds, almanac = convert_input(i_raw)
+	seed_ranges = seeds_to_ranges(seeds)
+	print(seeds)
+	print(seed_ranges)
 
-	total_sum = 0
+	processes = ['seed-to-soil map:', 'soil-to-fertilizer map:', 'fertilizer-to-water map:', 'water-to-light map:',
+				 'light-to-temperature map:', 'temperature-to-humidity map:', 'humidity-to-location map:']
 
-	for line in data:
-		print(line)
+	reverse_processes = ['humidity-to-location map:', 'temperature-to-humidity map:', 'light-to-temperature map:',
+						 'water-to-light map:', 'fertilizer-to-water map:', 'soil-to-fertilizer map:',
+						 'seed-to-soil map:']
 
-	return total_sum
+	for k, v in almanac.items():
+		print(f'{k}')
+		for p, q in v.items():
+			print(f'   {p}: {q}')
+
+	lowest = 1000000000000000
+
+	location = 0
+	seed_found = False
+
+	mask = 2**20 - 1
+
+	for loc in range(100000000, 1000000000):
+		if loc & mask == 0:
+			print(loc)
+
+		location = loc
+		for i in range(len(reverse_processes)):
+			# print(f'{loc} going into {reverse_processes[i]}')
+			loc = reverse_convert(almanac, reverse_processes[i], loc)
+			# print(f'   Result: {loc}')
+
+		# print('\n')
+
+		for r in range(len(seed_ranges)):
+			if seed_ranges[r][0] <= loc <= seed_ranges[r][1]:
+				print(f'Seed {loc} found in seed range!')
+				print(f'Corresponding location: {location}')
+				seed_found = True
+				break
+			# else:
+				# print(f'Seed {loc} not found in seed range: {seed_ranges[r]}.')
+
+		if seed_found:
+			break
+
+	# for num in seeds:
+	# 	for i in range(len(processes)):
+	# 		print(f'{num} going into {processes[i]}')
+	# 		num = convert_number(almanac, processes[i], num)
+	# 		print(f'   Result: {num}')
+	# 	print('\n')
+	# 	if num < lowest:
+	# 		lowest = num
+
+	return lowest
 
 
-total = main()
-print(f'Total: {total}')
+low = main()
+# print(f'Lowest: {low}')
--- dec05/input_handler.py
+++ dec05/input_handler.py
@@ -1,4 +1,34 @@
 def convert_input(data):
     data_strip = [i.strip('\n') for i in data]
+    seeds = data_strip.pop(0)
+    seeds_split = seeds.split(' ')
+    seeds_split.pop(0)
+    seeds_int = [int(x) for x in seeds_split]
 
-    return data_strip
+    almanac = {}
+
+    index = 0
+    key = ''
+    for i in range(len(data_strip)):
+        item = data_strip[i]
+        index += 1
+        if item == '':
+            continue
+        if item[0].isalpha():
+            key = item
+            almanac[item] = {}
+            index = 0
+        else:
+            item_list = item.split(' ')
+            start = int(item_list[1])
+            end = int(item_list[1]) + (int(item_list[2]) - 1)
+            modifier = int(item_list[0]) - int(item_list[1])
+            almanac[key][index] = {
+                'source_start': start,
+                'source_end': end,
+                'destination_start': start + modifier,
+                'destination_end': end + modifier,
+                'modifier': modifier
+            }
+
+    return seeds_int, almanac
