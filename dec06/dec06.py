5235a149f143bf97ce8c8270d3de3c051663c20e
Alex Eversmeyer <aeversme@gmail.com>
2023-12-07 18:02:52 -0700

    add dec06 solutions

--- dec06/dec06.py
+++ dec06/dec06.py
@@ -1,6 +1,23 @@
 from input_handler import convert_input
 
 
+def process_race(race):
+	time = race[0]
+	record = race[1]
+	winning_count = 0
+
+	for m in range(time):
+		speed = m
+		distance = speed * (time - m)
+		# print(f'   {m}; speed: {speed}, distance: {distance}')
+		# print(f'   Record: {record}')
+		if distance > record:
+			# print(f'      Winning race!')
+			winning_count += 1
+
+	return winning_count
+
+
 def main():
 	with open('input.txt') as i:
 		i_raw = i.readlines()
@@ -9,10 +26,26 @@ def main():
 
 	total_sum = 0
 
-	for line in data:
-		print(line)
+	races = []
 
-	return total_sum
+	# part 1
+	# for i in range(1):
+	# 	for j in range(1, len(data[i])):
+	# 		new_race = [int(data[i][j]), int(data[i + 1][j])]
+	# 		races.append(new_race)
+
+	# part 2
+	races = [[int(data[0][1] + data[0][2] + data[0][3] + data[0][4]), int(data[1][1] + data[1][2] + data[1][3] + data[1][4])]]
+
+	number = 1
+
+	for race in races:
+		print(race)
+		winning_count = process_race(race)
+		print(f'Winning count: {winning_count}')
+		number *= winning_count
+
+	return number
 
 
 total = main()
--- dec06/input_handler.py
+++ dec06/input_handler.py
@@ -1,4 +1,12 @@
 def convert_input(data):
     data_strip = [i.strip('\n') for i in data]
+    data_split = [i.split(' ') for i in data_strip]
+    new_data = []
+    for line in data_split:
+        new_line = []
+        for i in line:
+            if i:
+                new_line.append(i)
+        new_data.append(new_line)
 
-    return data_strip
+    return new_data
